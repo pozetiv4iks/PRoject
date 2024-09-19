ef8b64253bb4bf120435d123e70f6df489bcaca0
Stepan Duleuski <stepanduleuski@Lomrays-MacBook-Pro.local>
2024-09-19 12:58:48 +0300

    start project

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+
+function App() {
+    
+    return(
+    <>
+    
+    </>
+    )
+}
+
+export default App;
+
+// import { Router, Route } from "react-router-dom";
+// После чего поправить ваш App компонент следующим образом
+
+// const App = () => <div className="App">
+//   <Router>
+//     <div>
+//       <Route exact path="/" component={Home} />
+//       <Route exact path="/users" component={Users} />
+//       <Route exact path="/login" component={Login} />
+//     </div>
+//   </Router>
+// </div>;
