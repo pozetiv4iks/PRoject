7a025248121367c92cff9bcb769cb74e5313dd48
Stepan <137997706+pozetiv4iks@users.noreply.github.com>
2025-06-05 09:16:47 +0300

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./indeeeexxs.cssssсc"
+import "./indeeeexxs.csssssсc"
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
 
